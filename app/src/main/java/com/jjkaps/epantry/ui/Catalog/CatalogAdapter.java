376224661b69a1fe0fc59c050cfd78cc54d61d20
Kira <graves42@purdue.edu>
2020-07-27 13:27:02 -0400

    catalog filter toggle converted to dropdown, favorites filter added

--- app/src/main/java/com/jjkaps/epantry/ui/Catalog/CatalogAdapter.java
+++ app/src/main/java/com/jjkaps/epantry/ui/Catalog/CatalogAdapter.java
@@ -1,6 +1,7 @@
 package com.jjkaps.epantry.ui.Catalog;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,13 +17,15 @@ import com.jjkaps.epantry.R;
 import com.jjkaps.epantry.models.BarcodeProduct;
 import com.jjkaps.epantry.utils.Utils;
 
+
 import java.util.ArrayList;
+
+import static com.jjkaps.epantry.ui.Catalog.FilterType.NOTHING;
 
 public class CatalogAdapter extends ArrayAdapter<CatalogAdapterItem> implements Filterable {
     private ArrayList<CatalogAdapterItem> originalItems;
     private Context context;
-    private boolean scannedOnly;
-
+    private FilterType filter_type = NOTHING; //fave 2, scanned 1, none 0
     private static class ViewHolder {
         TextView itemCat;
     }
@@ -31,17 +34,13 @@ public class CatalogAdapter extends ArrayAdapter<CatalogAdapterItem> implements 
         super(context, 0, items);
         this.context = context;
         this.originalItems = items;
-        this.scannedOnly = false;
     }
 
-    public void toggleScannedOnly(String filter){
-        scannedOnly=!scannedOnly;
+    public void filterView(String filter, FilterType filter_type){
+        this.filter_type = filter_type;
         this.getFilter().filter(filter);
     }
 
-    public boolean isScannedOnly() {
-        return scannedOnly;
-    }
 
     @NonNull
     @Override
@@ -83,7 +82,6 @@ public class CatalogAdapter extends ArrayAdapter<CatalogAdapterItem> implements 
     }
 
     @NonNull
-    @Override
     public Filter getFilter() {
         if (filter == null)
             filter = new CatalogFilter(originalItems);
@@ -115,30 +113,61 @@ public class CatalogAdapter extends ArrayAdapter<CatalogAdapterItem> implements 
                 }
                 //following two lines is very important
                 //as publish result can only take FilterResults objects
-                filterScannedOnly(scannedOnly, tempList);
+                switch (filter_type) {
+                    case NOTHING:
+                        filterNothing(tempList);
+                        break;
+                    case SCANNED:
+                        filterScannedOnly(tempList);
+                        break;
+                    case FAVORITES:
+                        filterFaveOnly(tempList);
+                        break;
+                }
                 synchronized (this){
                     filterResults.values = tempList;
                     filterResults.count = tempList.size();
                 }
             }else {
                 tempList = new ArrayList<>(sources);
-                filterScannedOnly(scannedOnly, tempList);
-                filterResults.values = (!scannedOnly)?sources:tempList;
-                filterResults.count = (!scannedOnly)?sources.size():tempList.size();
+                switch (filter_type){
+                    case NOTHING:
+                        tempList = sources;
+                        break;
+                    case SCANNED:
+                        filterScannedOnly(tempList);
+                        break;
+                    case FAVORITES:
+                        filterFaveOnly(tempList);
+                        break;
+                }
+                filterResults.values = tempList;
+                filterResults.count = tempList.size();
+
             }
             return filterResults;
         }
-
-        private ArrayList<CatalogAdapterItem> filterScannedOnly(boolean scannedOnly, ArrayList<CatalogAdapterItem> tempItems) {
-            if(scannedOnly){
+        private ArrayList<CatalogAdapterItem> filterNothing(ArrayList<CatalogAdapterItem> tempItems) {
+            return tempItems;
+        }
+        private ArrayList<CatalogAdapterItem> filterScannedOnly(ArrayList<CatalogAdapterItem> tempItems) {
                 for (int i = tempItems.size()-1; i >= 0; i--) {
                     if(!Utils.isNotNullOrEmpty(tempItems.get(i).getBarcodeProduct().getBarcode())){
                         tempItems.remove(i);
                     }
                 }
-            }
             return tempItems;
         }
+        private ArrayList<CatalogAdapterItem> filterFaveOnly(ArrayList<CatalogAdapterItem> tempItems) {
+                for (int i = tempItems.size()-1; i >= 0; i--) {
+                    if(!(tempItems.get(i).getBarcodeProduct().getFavorite())){
+                        tempItems.remove(i);
+                    }
+                }
+            return tempItems;
+        }
+
+
 
         @Override
         protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
@@ -146,4 +175,6 @@ public class CatalogAdapter extends ArrayAdapter<CatalogAdapterItem> implements 
             notifyDataSetChanged();
         }
     }
+
+
 }
